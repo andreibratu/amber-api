55c6ca303bf348bb870777b5e32335d599e575b7
Andrei Bratu <bratu.andrei@outlook.com>
2018-04-19 08:30:53 +0300

    Fixed GeoService

--- services/GeoService.py
+++ services/GeoService.py
@@ -1,23 +1,26 @@
 from math import cos, radians
 
-LATITUDE_TO_KM = 111.23
-SEARCH_RADIUS_KM = 10
+LAT_TO_KM = 111.13
+LNG_TO_KM = 111.32
 
 
-def longitude_to_km(latitude):
-    return LATITUDE_TO_KM * cos(radians(latitude))
+def longitude_to_km(lng_delta, lat):
+    return lng_delta * LNG_TO_KM * cos(radians(lat))
 
 
-def latitude_to_km(latitude):
-    return latitude * LATITUDE_TO_KM
+def latitude_to_km(lat_delta):
+    return lat_delta * LAT_TO_KM
 
 
 class GeoService:
 
     @staticmethod
-    def filter_events_by_location(events, user_longitude, user_latitude):
-        return [x for x in events if (
-                longitude_to_km(abs(user_longitude - x.longitude)) ** 2 +
-                latitude_to_km(abs(user_latitude - x.latitude)) ** 2
-                < 10 ^ 2)
+    def filter_events_by_location(events, user_lng, user_lat, search_radius_km = 10):
+        events = [x for x in events if (
+                longitude_to_km(abs(user_lng - x.longitude)) ** 2 +
+                latitude_to_km(abs(user_lat - x.latitude)) ** 2
+                < search_radius_km ^ 2)
                 ]
+
+        return events
+
