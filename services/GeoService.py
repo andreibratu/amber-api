3dc31bd5378e8c2c86a9af5ac90fc19269294cd5
Andrei Bratu <bratu.andrei@outlook.com>
2018-05-03 10:27:49 +0300

    Refactored GeoService to a functional approach

--- services/GeoService.py
+++ services/GeoService.py
@@ -4,41 +4,15 @@ LAT_TO_KM = 111.13
 LNG_TO_KM = 111.32
 
 
-def longitude_to_km(lng_delta, lat):
-    return lng_delta * LNG_TO_KM * cos(radians(lat))
-
-
-def latitude_to_km(lat_delta):
-    return lat_delta * LAT_TO_KM
-
-
 class GeoService:
 
     @staticmethod
-    def filter_events_by_location(events, user_lng, user_lat, search_radius_km = 10):
-        print('Geo unfiltered: ', events)
+    def events_by_location_filter_builder(lng, lat, search_radius_km=10):
 
-        filtered_events = []
+        def lng_to_km(lng_delta): return abs(lng_delta) * LNG_TO_KM * cos(radians(lng_delta))
 
-        for x in events:
+        def lat_to_km(lat_delta): return abs(lat_delta) * LAT_TO_KM
 
-            print(user_lat, x.latitude)
-            print(user_lng, x.longitude)
+        def pythagoras(lng_km, lat_km, radius): return lng_km ** 2 + lat_km ** 2 <= radius ** 2
 
-            lat_delta = abs(user_lat-x.latitude)
-            lng_delta = abs(user_lng-x.longitude)
-
-            print(lng_delta, lat_delta)
-
-            lng_km = longitude_to_km(lng_delta, user_lat)
-            lat_km = latitude_to_km(lat_delta)
-
-            print(lng_km, lat_km)
-
-            if lng_km ** 2 + lat_km ** 2 <= search_radius_km ** 2:
-                filtered_events.append(x)
-
-        print('Geo filtered: ', filtered_events)
-
-        return filtered_events
-
+        return lambda event: pythagoras(lng_to_km(lng-event.longitude), lat_to_km(lat-event.latitude), search_radius_km)
